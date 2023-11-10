d56d7b5344116c5b617b27078ead4d2d9ac19cad
yuhanz1 <yuhanz9149@gmail.com>
2023-11-10 10:13:13 -0500

    1

---
+++ Yuhan-vite-project/js/main.js
@@ -0,0 +1,5 @@
+import '../css/style.css'; // ../ means to exit this folder (and, in this context, to enter css folder into style.css file)
+/* import { menu, name } from "./menu";
+
+console.log(menu, name); */
+
---
+++ Yuhan-vite-project/js/menu.js
@@ -0,0 +1,8 @@
+/* const menu = ["noodles", "pastry"];
+const name = ["sjkdfds"];
+
+export { menu, name }; */
+
+const aEvil = ["Abyssal Black Dragon", ""];
+const aGood = ["Demon-like Judge of Evil", "Scribe of Heaven"];
+
